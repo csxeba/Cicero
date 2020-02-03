5b82b4205b41c94edc79179e9db8a8c9274b42ee
Csaba Gór <csxeba@gmail.com>
2020-02-03 06:44:14 +0100

    Updated argparser helps and descriptions

--- simulate_tf.py
+++ simulate_tf.py
@@ -4,11 +4,25 @@ import numpy as np
 
 import cicero
 
-parser = argparse.ArgumentParser("Cicero - TensorFlow simulation", description="Toroidal Game of Life")
+descr = """Toroidal Game of Life.
 
-parser.add_argument("--num-simulations", default=100000, type=int)
-parser.add_argument("--max-steps", default=100, type=int)
-parser.add_argument("--convergence-detector-window", default=30, type=int)
+Use this script to run simulations in parallel.
+All simulations will produce an attractor candidate,
+which will be simply dumped to attractors.npy.
+
+This can be picked up by analyze_attractors.py, which
+will answer all homework questions.
+"""
+
+parser = argparse.ArgumentParser("Cicero - TensorFlow simulation",
+                                 description=descr)
+
+parser.add_argument("--num-simulations", default=100000, type=int,
+                    help="Number of parallel simulations")
+parser.add_argument("--max-steps", default=100, type=int,
+                    help="How many steps to run all simulations for")
+parser.add_argument("--convergence-detector-window", default=30, type=int,
+                    help="Slice size, on which convergence will be determined")
 
 arg = parser.parse_args()
 
