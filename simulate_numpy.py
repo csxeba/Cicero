6ca63d7191fb3b3bbf430b3559769f1956091c7c
Csaba Gór <csxeba@gmail.com>
2020-02-03 06:47:37 +0100

    Updated argparser helps and descriptions again

--- simulate_numpy.py
+++ simulate_numpy.py
@@ -22,14 +22,27 @@ def dispatch_user_defined_simulation(args):
 
 
 def main():
+    desr = """Toroidal Game of Life
+    
+    Use this script to execute one-shot simulations.
+    
+    """
+
     parser = ArgumentParser("Cicero - NumPy simulation", description="Toroidal Game of Life")
-    parser.add_argument("--initial-state", default="random", type=str)
-    parser.add_argument("--convergence-detector-window", default=30, type=int)
-    parser.add_argument("--break-on-convergence", default=False, type=bool)
-    parser.add_argument("--replay-fps", default=5, type=int)
-    parser.add_argument("--init-probability", default=None, type=int)
-    parser.add_argument("--size", default="6x6", type=str)
-    parser.add_argument("--max-steps", default=100, type=int)
+    parser.add_argument("--initial-state", default="random", type=str,
+                        help="Either a JSON file path or 'random'")
+    parser.add_argument("--convergence-detector-window", default=30, type=int,
+                        help="Slice size, on which convergence will be determined")
+    parser.add_argument("--break-on-convergence", default=False, type=bool,
+                        help="Whether to end the simulation on convergence")
+    parser.add_argument("--replay-fps", default=5, type=int,
+                        help="Replay speed in frames per second")
+    parser.add_argument("--init-probability", default=None, type=int,
+                        help="If set, the random initialization will produce active cells in this rate")
+    parser.add_argument("--size", default="6x6", type=str,
+                        help="Simulation size, defaults to 6x6")
+    parser.add_argument("--max-steps", default=100, type=int,
+                        help="How many steps to run the simulation for, if 'break-on-convergence' is False.")
 
     args = parser.parse_args()
     width, height = map(int, args.size.split("x"))
